2c5c66a3c165e24e78cc489355060cfab07e2a45
gritin5 <santiago1927123@gmail.com>
2021-12-07 00:04:26 -0500

    ULTIMA ACTUALIZACION 7/12/2021

--- app/src/main/java/com/example/appg9/utilities/Utilities.java
+++ app/src/main/java/com/example/appg9/utilities/Utilities.java
@@ -10,6 +10,7 @@ public class Utilities {
     public static final String CAMPO_CONTACT = "contact";
     public static final String CAMPO_VALUE = "value";
 
+
     public static final String CREAR_TABLA_EVENTOS = "CREATE TABLE " + TABLA_EVENTS + "("+ CAMPO_IMG + " INTEGER, " + CAMPO_EVENT + " TEXT, " + CAMPO_TYPE + " TEXT, "+ CAMPO_DATE + " TEXT, "
     + CAMPO_TIME + " TEXT, " + CAMPO_CONTACT + " TEXT, " + CAMPO_VALUE + " INTEGER)";
 
